1b5652cf38857ef9514b23ea8f9bc8665e3dec14
Bilal Alp <symen46@gmail.com>
2015-09-22 23:46:05 +0300

    Maven repository configuration updated.

--- cxfloggerconsumer/src/main/java/com/w3schools/webservices/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.w3schools.com/webservices/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.w3schools.webservices;
